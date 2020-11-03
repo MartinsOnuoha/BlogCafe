d36ce9a33cc492159b04ad347fd2ae4204d61584
MartinsOnuoha <martinsvictor.nd@gmail.com>
2020-11-03 16:26:50 +0100

    update createPost.vue

--- resources/js/components/CreatePost.vue
+++ resources/js/components/CreatePost.vue
@@ -1,28 +1,32 @@
 <template>
-  <div class="card mt-4" :key="componentKey">
-    <div class="card-header">New Post</div>
+  <div :key="componentKey" class="card mt-4">
+    <div class="card-header">
+      New Post
+    </div>
     <div class="card-body">
       <div
         v-if="status_msg"
         :class="{ 'alert-success': status, 'alert-danger': !status }"
         class="alert"
         role="alert"
-      >{{ status_msg }}</div>
+      >
+        {{ status_msg }}
+      </div>
       <form>
         <div class="form-group">
           <label for="exampleFormControlInput1">Title</label>
           <input
+            id="title"
             v-model="title"
             type="text"
             class="form-control"
-            id="title"
             placeholder="Post Title"
             required
-          />
+          >
         </div>
         <div class="form-group">
           <label for="exampleFormControlTextarea1">Post Content</label>
-          <textarea v-model="body" class="form-control" id="post-content" rows="3" required></textarea>
+          <textarea id="post-content" v-model="body" class="form-control" rows="3" required />
         </div>
         <div class>
           <el-upload
@@ -32,10 +36,10 @@
             :on-change="updateImageList"
             :auto-upload="false"
           >
-            <i class="el-icon-plus"></i>
+            <i class="el-icon-plus" />
           </el-upload>
           <el-dialog :visible.sync="dialogVisible">
-            <img width="100%" :src="dialogImageUrl" alt />
+            <img width="100%" :src="dialogImageUrl" alt>
           </el-dialog>
         </div>
       </form>
@@ -43,9 +47,11 @@
     <div class="card-footer">
       <button
         type="button"
-        @click="createPost"
         class="btn btn-success"
-      >{{ isCreatingPost ? "Posting..." : "Create Post" }}</button>
+        @click="createPost"
+      >
+        {{ isCreatingPost ? "Posting..." : "Create Post" }}
+      </button>
     </div>
   </div>
 </template>
@@ -77,92 +83,90 @@
 </style>
 
 <script>
-import { setTimeout } from "timers";
-import { mapState, mapActions } from "vuex";
+import { mapActions } from 'vuex'
 
 export default {
-  name: "create-post",
-  props: ["posts"],
-  data() {
+  name: 'CreatePost',
+  data () {
     return {
-      dialogImageUrl: "",
+      dialogImageUrl: '',
       dialogVisible: false,
       imageList: [],
-      status_msg: "",
-      status: "",
+      status_msg: '',
+      status: '',
       isCreatingPost: false,
-      title: "",
-      body: "",
+      title: '',
+      body: '',
       componentKey: 0
-    };
+    }
   },
   computed: {},
-  mounted() {},
+  mounted () {},
   methods: {
-    ...mapActions(["getAllPosts"]),
-    updateImageList(file) {
-      this.imageList.push(file.raw);
+    ...mapActions(['getAllPosts']),
+    updateImageList (file) {
+      this.imageList.push(file.raw)
     },
-    handlePictureCardPreview(file) {
-      this.dialogImageUrl = file.url;
-      this.imageList.push(file);
-      this.dialogVisible = true;
+    handlePictureCardPreview (file) {
+      this.dialogImageUrl = file.url
+      this.imageList.push(file)
+      this.dialogVisible = true
     },
-    createPost(e) {
-      e.preventDefault();
+    createPost (e) {
+      e.preventDefault()
       if (!this.validateForm()) {
-        return false;
+        return false
       }
-      const that = this;
-      this.isCreatingPost = true;
-      let formData = new FormData();
+      const that = this
+      this.isCreatingPost = true
+      const formData = new FormData()
 
-      formData.append("title", this.title);
-      formData.append("body", this.body);
+      formData.append('title', this.title)
+      formData.append('body', this.body)
 
-      $.each(this.imageList, function(key, image) {
-        formData.append(`images[${key}]`, image);
-      });
+      $.each(this.imageList, function (key, image) {
+        formData.append(`images[${key}]`, image)
+      })
 
       api
-        .post("/post/create_post", formData, {
-          headers: { "Content-Type": "multipart/form-data" }
+        .post('/post/create_post', formData, {
+          headers: { 'Content-Type': 'multipart/form-data' }
         })
-        .then(res => {
-          this.title = this.body = "";
-          this.status = true;
-          this.showNotification("Post Successfully Created");
-          this.isCreatingPost = false;
-          this.imageList = [];
+        .then((res) => {
+          this.title = this.body = ''
+          this.status = true
+          this.showNotification('Post Successfully Created')
+          this.isCreatingPost = false
+          this.imageList = []
           /*
            this.getAllPosts() can be used here as well
            note: "that" has been assigned the value of "this" at the top
            to avoid context related issues.
            */
-          that.getAllPosts();
-          that.componentKey += 1;
-        });
+          that.getAllPosts()
+          that.componentKey += 1
+        })
     },
-    validateForm() {
-      //no vaildation for images - it is needed
+    validateForm () {
+      // no vaildation for images - it is needed
       if (!this.title) {
-        this.status = false;
-        this.showNotification("Post title cannot be empty");
-        return false;
+        this.status = false
+        this.showNotification('Post title cannot be empty')
+        return false
       }
       if (!this.body) {
-        this.status = false;
-        this.showNotification("Post body cannot be empty");
-        return false;
+        this.status = false
+        this.showNotification('Post body cannot be empty')
+        return false
       }
-      return true;
+      return true
     },
-    showNotification(message) {
-      this.status_msg = message;
+    showNotification (message) {
+      this.status_msg = message
       setTimeout(() => {
-        this.status_msg = "";
-      }, 3000);
+        this.status_msg = ''
+      }, 3000)
     }
   }
-};
+}
 </script>
